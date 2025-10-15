ac4ea7c0558b4d1e8b590638d59e403f6a9f751c
AhkamKhallaf <31520379+AhkamKhallaf@users.noreply.github.com>
2025-10-15 09:26:26 +0300

    [11] [Time Beats: 9.09%] [Memory Beats: 56.97%] - LeetPush

---
+++   sliding_window pattern/Container-With-Most-Water.dart
@@ -0,0 +1,25 @@
+ class Solution {
+  int maxArea(List<int> height) {
+  int left = 0;
+  int right = height.length - 1;
+  int maxArea = 0;
+
+  while (left < right) {
+    int width = right - left;
+    int h = height[left] < height[right] ? height[left] : height[right];
+    int area = width * h;
+    if (area > maxArea) maxArea = area;
+
+ 
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+}
+ 
+
+}
