997c9750c665f0e471a0a36f4034b7903ab75c5d
AhkamKhallaf <31520379+AhkamKhallaf@users.noreply.github.com>
2025-12-09 21:55:19 +0200

    [141] [Time Beats: 100.00%] [Memory Beats: 14.51%] - LeetPush

---
+++  fast and slow pointers/Linked-List-Cycle.java
@@ -0,0 +1,32 @@
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14       
+15        if (head == null) return false;
+16        
+17        ListNode slow = head;
+18        ListNode fast = head;
+19
+20        while (fast != null && fast.next != null) {
+21            slow = slow.next;     
+22            fast = fast.next.next;  
+23
+24            if (slow == fast) {
+25                return true; 
+26            }
+27        }
+28
+29        return false;
+30    
+31    }
+32}
